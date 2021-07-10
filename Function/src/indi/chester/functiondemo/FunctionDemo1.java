31a0b48f319679940b99b4a13032e179cbc19c7c
chesterzhang <chesterzhang666@gmail.com>
2021-07-10 19:01:24 +0800

    load in IDEA

---
+++ Function/src/indi/chester/functiondemo/FunctionDemo1.java
@@ -0,0 +1,46 @@
+package indi.chester.functiondemo;
+
+public class FunctionDemo1 {
+
+    //方法定义: 访问限制符 返回类型 方法名(参数1, 参数2, ...）
+    public  int plus(int a, int b) {
+        return a+b;
+    }
+
+    //方法重载: 除了参数必须不同,其他可以相同,其意义在于: 让一个方法在同一个类里面有不同的作用
+    public  float plus(float a, float b) {
+        return a+b;
+    }
+
+    //可变参数列表作为参数, 在许多int 型数据a 中寻找 n
+    public void  search(int n, int... a) {
+        boolean flag=false;
+        for(int a1:a) {
+            if (a1==n) {
+                flag=true;
+                break;
+            }
+        }
+        if (flag) {
+            System.out.println("找到了 "+n);
+        }else {
+            System.out.println("没找到 "+n);
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        int a=1, b=2;
+        FunctionDemo1 fd=new FunctionDemo1();
+        System.out.println("1+2 = "+fd.plus(a,b));
+
+        float c=1.0f, d=2.0f;
+        System.out.println("1.0+2.0 = "+fd.plus(a,b));
+
+        int[] arr={5,4,3,2,1,0};
+        fd.search(1, arr);
+        fd.search(1, 5,4,3,2,1,0);
+    }
+
+}
