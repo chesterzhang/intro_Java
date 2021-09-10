42637c5ce751ccc6264903952510225eb38c9ff8
chesterzhang <chesterzhang666@gmail.com>
2021-09-10 22:29:20 +0800

    added Maven0 folder

---
+++ Maven0/src/main/java/indi/chester/App.java
@@ -0,0 +1,13 @@
+package indi.chester;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
