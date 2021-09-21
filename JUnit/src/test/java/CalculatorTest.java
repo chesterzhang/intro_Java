8ae901460aa5110135bd10020a61623718bcff55
chesterzhang <chesterzhang666@gmail.com>
2021-09-21 22:09:47 +0800

    updated JUnit

--- JUnit/src/test/java/CalculatorTest.java
+++ JUnit/src/test/java/CalculatorTest.java
@@ -1,14 +1,18 @@
 import org.junit.Test;
 
-
 public class CalculatorTest {
-
+    private Calculator cal=new Calculator();
 
     @Test
-    public void add() {
+    public void testAdd(){
+        int result=cal.add(1,2);
+        System.out.println(result);
     }
 
     @Test
-    public void divide() {
+    public void testDivide(){
+        float result=cal.divide(1,0);
+        System.out.println(result);
     }
+
 }
