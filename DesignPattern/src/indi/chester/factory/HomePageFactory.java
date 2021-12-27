108e44ae8fdd88f2061b346ca66d9b8990ee7207
chesterzhang <chesterzhang666@gmail.com>
2021-12-27 10:28:27 +0800

    updated factory pattern

--- DesignPattern/src/indi/chester/factory/HomePageFactory.java
+++ DesignPattern/src/indi/chester/factory/HomePageFactory.java
@@ -2,12 +2,14 @@ package indi.chester.factory;
 
 public class HomePageFactory {
     public static IHomePage getHomePage(String country){
-        if (country.equals("China")){
-            return new ChineseHomePage();
-        }else if (country.equals("USA")){
-            return new EnglishHomePage();
-        }else {
-            return null;
+        switch (country){
+            case "China":
+                return new ChineseHomePage();
+            case "USA":
+                return new EnglishHomePage();
+            default:
+                return null;
         }
+
     }
 }
