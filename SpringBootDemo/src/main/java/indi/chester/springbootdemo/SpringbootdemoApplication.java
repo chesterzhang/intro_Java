88cb552123fb758db32cb0e56d760041ef310170
chesterzhang <chesterzhang666@gmail.com>
2022-01-18 20:57:14 +0800

    added SpringBootDemo

---
+++ SpringBootDemo/src/main/java/indi/chester/springbootdemo/SpringbootdemoApplication.java
@@ -0,0 +1,13 @@
+package indi.chester.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootdemoApplication.class, args);
+    }
+
+}
